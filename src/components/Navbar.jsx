2f36ae75f74de27b088d2a0ff7f932f159bbda70
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-06-17 10:55:56 +0200

    Initialize repo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl text-[#005637] p-4">
+        Welcome to Chamabondo
+      </h1>
+      <h2 className="text-center text-[#FBF503] text-3xl my-8 uppercase">
+        Primary school
+      </h2>
+    </div>
+  );
+};
+
+export default Navbar;
